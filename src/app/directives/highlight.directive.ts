956f5b25668435c3483129108a5ccf01ea706d8b
RajVinoth2526 <vinothraj2526@gmail.com>
2023-05-01 20:53:06 +0530

    commit message

---
+++ src/app/directives/highlight.directive.ts
@@ -0,0 +1,37 @@
+import { Directive ,ElementRef,Input,OnChanges,SimpleChanges} from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective implements OnChanges {
+  @Input() hightlightText = '';
+  @Input()
+  originalHTML = '';
+
+  constructor(private el : ElementRef) {
+   
+   }
+
+   ngOnChanges(changes : SimpleChanges){
+
+    console.log(changes);
+
+    const {currentValue,firstChange} = changes['hightlightText'];
+    if(firstChange){
+      this.originalHTML = this.el.nativeElement.innerHTML;
+      return;
+    }
+    if(currentValue){
+      const regexp = new RegExp(`(${currentValue})`,'gi');
+      this.el.nativeElement.innerHTML = this.originalHTML.replace(
+        regexp,
+       `<span style = "background-color : yellow">\$1</span>`
+
+      );
+      
+    }else{
+      this.el.nativeElement.innerHTML = this.originalHTML;
+    }
+
+   }
+}
